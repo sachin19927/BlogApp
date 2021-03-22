4250ec8bb8a227971c7f65c9845e4ad24070c9e3
SACHIN HS <sachin19927@gmail.com>
2021-03-22 06:57:31 +0530

    base commet change

--- src/main/java/com/blog/crm/BlogAppApplication.java
+++ src/main/java/com/blog/crm/BlogAppApplication.java
@@ -36,9 +36,9 @@ public class BlogAppApplication extends SpringBootServletInitializer {
 	 * @param args accepts command line arguments
 	 */
 	public static void main(String[] args) {
-		GenericLogger.debug(ModuleName.LOGGER, BlogAppApplication.class, "Blog App Load");
+		GenericLogger.debug(ModuleName.LOGGER, BlogAppApplication.class, "Blog App Load: v2");
 		SpringApplication.run(BlogAppApplication.class, args);
-		GenericLogger.debug(ModuleName.LOGGER, BlogAppApplication.class, "Blog App Loaded");
+		GenericLogger.debug(ModuleName.LOGGER, BlogAppApplication.class, "Blog App Loaded complete : v3");
 	}
 
 }
